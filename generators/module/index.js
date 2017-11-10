6413c32090aee1cbf19588b8313757c3582fea03
Eldar Aliyev <eldar.aliyev8@gmail.com>
2017-11-10 21:20:44 +0200

    Refactor module generator

--- generators/module/index.js
+++ generators/module/index.js
@@ -1,20 +1,26 @@
-'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
-var _ = require('lodash');
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
+const path = require('path');
 
-var path = require('path');
+const MODULE_ROOT = 'src/modules';
 
-var moduleRoot = 'src/modules';
+const color = {
+  b: chalk.bold,
+  i: chalk.italic,
+  yb: chalk.yellow.bold,
+  gb: chalk.green.bold,
+  bi: chalk.blue.italic
+};
 
-module.exports = yeoman.Base.extend({
-  _parseName: function () {
-    this.moduleName = this.name.toLowerCase().trim()
-      .replace(/[\W]+/g, '-'); // Replace any character except [^a-zA-Z0-9_] with a dash
-  },
-  constructor: function () {
-    yeoman.Base.apply(this, arguments);
+module.exports = class extends Generator {
+  _parseName() {
+    // Transform to kebab-case
+    this.moduleName = this.options.name.replace(/[A-Z]/g, ' $&').toLowerCase().trim().replace(/[\W]+/g, '-');
+  }
+
+  constructor(args, opts) {
+    super(args, opts);
 
     // Cli arguments
     this.argument('name', {
@@ -26,53 +32,42 @@ module.exports = yeoman.Base.extend({
 
     // Cli options
     this.option('quiet', {
-      desc: 'Don\'t ask, use default parameters.',
+      desc: `Don't ask, use default parameters.`,
       alias: 'q',
       type: Boolean
     });
-  },
-  prompting: function () {
+  }
+
+  prompting() {
     // Greet the user.
     this.log(yosay(
-      'We are creating the ' +
-      chalk.green.bold(this.moduleName) +
-      ' module inside ' +
-      chalk.blue.italic(path.join(moduleRoot, this.moduleName)) +
-      ' directory!'
+      `We are creating the ${color.gb(this.moduleName)} module inside ${color.bi(path.join(MODULE_ROOT, this.moduleName))} directory!`
     ));
 
-    var prompts = [];
+    const prompts = [];
 
     return this.prompt(prompts).then(function (props) {
       // To access props later use this.props.someAnswer;
       this.props = props;
     }.bind(this));
-  },
+  }
 
-  writing: function () {
+  writing() {
     // Copy templates
     this.fs.copyTpl(
       // Globs: https://github.com/isaacs/node-glob#glob-primer
       this.templatePath('**/*.?(js|jsx)'),
-      this.destinationPath(path.join(moduleRoot, this.moduleName)),
+      this.destinationPath(path.join(MODULE_ROOT, this.moduleName)),
       {
         name: this.moduleName
       }
     );
-  },
-
-  end: function () {
-    // Notification
-    this.log(
-      '- ' +
-      chalk.yellow.bold('Don\'t forget ') +
-      'to ' +
-      chalk.bold('plug in ') +
-      chalk.italic(this.moduleName) +
-      '\'s ' +
-      chalk.green.bold('reducers') +
-      ' inside your root reducer!'
-    );
   }
 
-});
+  end() {
+    // Notification
+    this.log(
+      `- ${color.yb('Don\'t forget ')} to ${color.b('plug in ')}${color.i(this.moduleName)}'s ${color.gb('reducers')} inside your root reducer!`
+    );
+  }
+};
