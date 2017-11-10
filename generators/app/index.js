a12308ee31ffb80e1f6cf4551b6f13d76a7f4747
Eldar Aliyev <eldar.aliyev8@gmail.com>
2017-11-10 20:41:58 +0200

    Update dependencies, refactor app generator

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,23 +1,20 @@
 'use strict';
-var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
+const Generator = require('yeoman-generator');
+const chalk = require('chalk');
+const yosay = require('yosay');
 
-var DELAY = 2000;
+const DELAY = 2000;
 
-function delayMessage(message, delay, context) {
-  return new Promise(function (resolve, reject) { // eslint-disable-line no-unused-vars
-    setTimeout(function () {
-      context.log(message);
-      return resolve();
-    }, delay || 1000);
-  });
-}
+const delayMessage = (message, delay, context) => new Promise(resolve => {
+  setTimeout(() => {
+    context.log(message);
+    return resolve();
+  }, delay || 1000);
+});
 
-module.exports = yeoman.Base.extend({
-  constructor: function () {
-    // super
-    yeoman.Base.apply(this, arguments);
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
 
     this.option('noshit', {
       desc: 'No delay for shit messages.',
@@ -27,58 +24,82 @@ module.exports = yeoman.Base.extend({
     });
     this.shit = !this.options.noshit;
     this.delay = DELAY;
-  },
-  saveRoot: function () {
+  };
+
+  saveRoot() {
     this.config.save();
     this.log('Current directory marked as root for further generator invocations.' +
       '\nTo reset it just remove ' +
       chalk.red('.jo-rc.json') +
       ' file from this directory.');
-  },
-  prompting: {
-    greeting: function () {
-      this.log(yosay(
-        'We are creating the ' + chalk.red('React/Redux') + ' web app!'
-      ));
-    },
+  }
 
-    askMortality: function () {
-      var prompts = [{
+  prompting() {
+    this._greeting();
+
+    return this._askMortality()
+      .then(() => this.props.mortality || this._wrongAnswer())
+      .then(() => this.props.mortality && this._disableMortality())
+      .then(() => this.props.mortality && this._dramaticPause())
+      .then(() => this.props.mortality && this._sorry())
+      .then(() => this.props.mortality && this._finish())
+  };
+
+  install() {
+    // this.installDependencies();
+  }
+
+  /* ================= Private stuff ================= */
+
+  _greeting() {
+    this.log(yosay(
+      'We are creating the ' + chalk.red('React/Redux') + ' web app!'
+    ));
+  }
+
+  _askMortality() {
+    const prompts = [
+      {
         type: 'confirm',
         name: 'mortality',
         message: 'Would you like to live forever?',
         default: true,
         store: true // save the answer for future use
-      }];
-
-      return this.prompt(prompts).then(function (props) {
-        // To access props later use this.props.mortality;
-        this.props = props;
-      }.bind(this));
-    },
-
-    disableMortality: function () {
-      if (this.shit) {
-        this.log(chalk.blue.italic('Trying to disable the mortality gene from your DNA...'));
       }
-    },
-    dramaticPause: function () {
-      return this.shit ?
-        delayMessage(chalk.blue.italic('Operation failed.'), this.delay, this) : null;
-    },
-    sorry: function () {
-      return this.shit ?
-        delayMessage(chalk.blue.italic('Mortality gene is read-only. Wrong species detected.'),
-          this.delay, this) : null;
-    },
-    finish: function () {
-      return this.shit ?
-        delayMessage(chalk.blue.italic('---'),
-          this.delay, this) : null;
-    }
-  },
+    ];
 
-  install: function () {
-    // this.installDependencies();
+    return this.prompt(prompts).then((props) => {
+      // To access props later use this.props.mortality;
+      this.props = props;
+    });
   }
-});
+
+  _wrongAnswer() {
+    this.log(chalk.red.italic('That was pretty stupid choice.'));
+  }
+
+  _disableMortality() {
+    if (this.shit) {
+      this.log(chalk.blue.italic('Trying to disable the mortality gene from your DNA...'));
+    }
+  }
+
+  _dramaticPause() {
+    return this.shit
+      ? delayMessage(chalk.blue.italic('Operation failed.'), this.delay, this)
+      : null;
+  }
+
+  _sorry() {
+    return this.shit
+      ? delayMessage(chalk.blue.italic('Mortality gene is read-only. Wrong species detected.'),
+        this.delay, this)
+      : null;
+  }
+
+  _finish() {
+    return this.shit ? delayMessage(chalk.blue.italic('---'),
+      this.delay, this) : null;
+  }
+
+};
